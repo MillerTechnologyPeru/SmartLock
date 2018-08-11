a40cbcb14aa57a341e8a5abc3c7605c8e828e8fb
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-08-11 18:16:26 -0500

    Working on Key

--- Sources/CoreLock/Key.swift
+++ Sources/CoreLock/Key.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// A smart lock key.
 public struct Key {
     
     public let identifier: UUID
@@ -14,4 +15,43 @@ public struct Key {
     public let name: String
     
     public let permission: Permission
+    
+    public init(identifier: UUID = UUID(),
+                name: String = "",
+                permission: Permission) {
+        
+        self.identifier = identifier
+        self.name = name
+        self.permission = permission
+    }
+}
+
+// MARK: - Codable
+
+extension Key: Codable {
+    
+    public enum CodingKeys: String, CodingKey {
+        
+        case identifier
+        case permission
+        case name
+    }
+    
+    public init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.identifier = try container.decode(UUID.self, forKey: .identifier)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.permission = try container.decode(Permission.self, forKey: .permission)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(identifier, forKey: .identifier)
+        try container.encode(name, forKey: .name)
+        try container.encode(permission, forKey: .permission)
+    }
 }
