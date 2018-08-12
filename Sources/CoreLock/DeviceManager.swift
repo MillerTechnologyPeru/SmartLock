43e73600f60df7ecce91a679fc3d105e6273aa73
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-08-11 23:58:08 -0500

    Working on `DeviceManager`

--- Sources/CoreLock/DeviceManager.swift
+++ Sources/CoreLock/DeviceManager.swift
@@ -42,13 +42,55 @@ public final class SmartLockManager <Central: CentralProtocol> {
     ///
     /// - Parameter event: Callback for a found device.
     public func scan(duration: TimeInterval,
-                     timeout: TimeInterval,
-                     filterDuplicates: Bool = true) throws {
+                     filterDuplicates: Bool = true,
+                     event: ((Peripheral) -> ())? = nil) throws {
         
-        let scanResults = try central.scan(duration: duration, filterDuplicates: filterDuplicates)
+        let start = Date()
+        
+        let end = start + duration
+        
+        try self.scan(filterDuplicates: filterDuplicates, event: event, scanMore: { Date() < end  })
+    }
+    
+    /// Scans for nearby devices.
+    ///
+    /// - Parameter event: Callback for a found device.
+    ///
+    /// - Parameter scanMore: Callback for determining whether the manager
+    /// should continue scanning for more devices.
+    public func scan(filterDuplicates: Bool = true,
+                     event: ((Peripheral) -> ())? = nil,
+                     scanMore: () -> (Bool)) throws {
+        
+        var foundPeripherals = Set<Peripheral>()
+        
+        var foundLocks = Set<Peripheral>()
+        
+        try self.central.scan(filterDuplicates: filterDuplicates, shouldContinueScanning: scanMore) { [unowned self] (scanData) in
+            
+            foundPeripherals.insert(scanData.peripheral)
+            
+            scanData.advertisementData
+            
+            // filter peripheral
+            //guard let foundDevice = self.found(peripheral: scanData)
+            //    else { return }
+            
+            event?(scanData.peripheral)
+        }
+        
+        let foundDevicesCount = foundLocks.count
+        
+        if foundDevicesCount > 0 { self.log?("Found \(foundPeripherals.count) peripherals (\(foundLocks.count) Locks)") }
     }
     
     // MARK: - Private Methods
     
     
+}
+
+/// Lock Peripheral
+public struct LockPeripheral <Peripheral: Peer> {
+    
+    public let peripheral: Peripheral
 }
