3f9567ca0549e55213ffa3e0ef30d38433a98391
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-05-20 02:05:54 -0500

    Updated `AuthorizationStoreFile`

--- Sources/CoreLockGATTServer/AuthorizationStoreFile.swift
+++ Sources/CoreLockGATTServer/AuthorizationStoreFile.swift
@@ -12,6 +12,15 @@ import CoreLock
 
 public final class AuthorizationStoreFile: LockAuthorizationStore {
     
+    // MARK: - Properties
+    
+    public let url: URL
+    
+    private let jsonDecoder = JSONDecoder()
+    private let jsonEncoder = JSONEncoder()
+    
+    private var database = Database()
+    
     public init(url: URL) {
         
         self.url = url
@@ -20,12 +29,26 @@ public final class AuthorizationStoreFile: LockAuthorizationStore {
         loadData()
     }
     
-    public let url: URL
+    // MARK: - Methods
     
-    private let jsonDecoder = JSONDecoder()
-    private let jsonEncoder = JSONEncoder()
+    public var isEmpty: Bool {
+        
+        return database.keys.isEmpty
+            && database.newKeys.isEmpty
+    }
     
-    private var database = Database()
+    public func add(_ key: Key, secret: KeyData) throws {
+        
+        try write { $0.keys.append(Database.KeyEntry(key: key, secret: secret)) }
+    }
+    
+    public func key(for identifier: UUID) throws -> (key: Key, secret: KeyData)? {
+        
+        guard let keyEntry = database.keys.first(where: { $0.key.identifier == identifier })
+            else { return nil }
+        
+        return (keyEntry.key, keyEntry.secret)
+    }
     
     private func loadData() {
         
@@ -48,30 +71,13 @@ public final class AuthorizationStoreFile: LockAuthorizationStore {
         
         self.database = database
     }
-    
-    public var isEmpty: Bool {
-        
-        return database.keys.isEmpty
-            && database.newKeys.isEmpty
-    }
-    
-    public func add(_ key: Key, secret: KeyData) throws {
-        
-        try write { $0.keys.append(Database.KeyEntry(key: key, secret: secret)) }
-    }
-    
-    public func key(for identifier: UUID) throws -> (key: Key, secret: KeyData)? {
-        
-        guard let keyEntry = database.keys.first(where: { $0.key.identifier == identifier })
-            else { return nil }
-        
-        return (keyEntry.key, keyEntry.secret)
-    }
 }
+
+// MARK: - Supporting Types
 
 private extension AuthorizationStoreFile {
     
-    struct Database {
+    struct Database: Codable, Equatable {
         
         var keys: [KeyEntry]
         
@@ -87,110 +93,18 @@ private extension AuthorizationStoreFile {
 
 extension AuthorizationStoreFile.Database {
     
-    struct KeyEntry {
+    struct KeyEntry: Codable, Equatable {
         
         let key: Key
-        
         let secret: KeyData
     }
     
-    struct NewKeyEntry {
+    struct NewKeyEntry: Codable, Equatable {
         
         /// New key
         let newKey: Key
         
         /// Shared secret used for onboarding
         let sharedSecret: KeyData
-    }
-}
-
-extension AuthorizationStoreFile.Database: Codable {
-    
-    internal enum CodingKeys: String, CodingKey {
-        
-        case keys
-        case newKeys
-    }
-    
-    public init(from decoder: Decoder) throws {
-        
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.keys = try container.decode([KeyEntry].self, forKey: .keys)
-        self.newKeys = try container.decode([NewKeyEntry].self, forKey: .newKeys)
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(keys, forKey: .keys)
-        try container.encode(newKeys, forKey: .newKeys)
-    }
-}
-
-extension AuthorizationStoreFile.Database.KeyEntry: Codable {
-    
-    internal enum CodingKeys: String, CodingKey {
-        
-        case key
-        case secret
-    }
-    
-    public init(from decoder: Decoder) throws {
-        
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.key = try container.decode(Key.self, forKey: .key)
-        
-        let data = try container.decode(Data.self, forKey: .secret)
-        
-        guard let secret = KeyData(data: data) else {
-            
-            throw DecodingError.typeMismatch(KeyData.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Could not initialize key data"))
-        }
-        
-        self.secret = secret
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(key, forKey: .key)
-        try container.encode(secret.data, forKey: .secret)
-    }
-}
-
-extension AuthorizationStoreFile.Database.NewKeyEntry: Codable {
-    
-    internal enum CodingKeys: String, CodingKey {
-        
-        case newKey
-        case sharedSecret
-    }
-    
-    public init(from decoder: Decoder) throws {
-        
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        
-        self.newKey = try container.decode(Key.self, forKey: .newKey)
-        
-        let data = try container.decode(Data.self, forKey: .sharedSecret)
-        
-        guard let secret = KeyData(data: data) else {
-            
-            throw DecodingError.typeMismatch(KeyData.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Could not initialize key data from \(data)"))
-        }
-        
-        self.sharedSecret = secret
-    }
-    
-    public func encode(to encoder: Encoder) throws {
-        
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(newKey, forKey: .newKey)
-        try container.encode(sharedSecret.data, forKey: .sharedSecret)
     }
 }
