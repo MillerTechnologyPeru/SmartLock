1401c4c839079d4d45bfa42fdf4bef1993b5cd8d
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-07-01 23:46:42 -0500

    Working on Debian package

--- debianpackage/swift.sh
+++ debianpackage/swift.sh
@@ -2,7 +2,13 @@
 export PATH=/opt/colemancda/swift/usr/bin:"${PATH}"
 
 # Install Swift
+echo "Extracting Swift"
 tar xvf swift.tar
+echo "Installing Swift"
+rm -rf /opt/colemancda/swift/*
+cp -rf ./usr /opt/colemancda/swift/
+echo "Copying Swift to package"
 rm -rf ./lockd/opt/colemancda/swift/*
 cp -rf ./usr ./lockd/opt/colemancda/swift/
+echo "Cleanup extracted files"
 rm -rf ./usr
