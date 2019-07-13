03d0aa22a451624c52faa7aa52827d93ebbede66
JurjDev <jurjdev@gmail.com>
2019-07-13 01:21:20 +0100

    Working on RasspberryPi

--- Sources/lockd/GPIO.swift
+++ Sources/lockd/GPIO.swift
@@ -43,6 +43,8 @@ public extension LockHardware {
         switch model {
         case .orangePiOne:
             return OrangePiOneGPIO()
+        case .raspberryPi3:
+            return RaspberryPi3GPIO()
         default:
             return nil
         }
@@ -85,6 +87,50 @@ public final class OrangePiOneGPIO: LockGPIOController {
         set { relayGPIO.value = newValue.rawValue }
     }
         
+    public var led: GPIOState {
+        get { return GPIOState(rawValue: ledGPIO.value) ?? .off }
+        set { ledGPIO.value = newValue.rawValue }
+    }
+    
+    public var didPressResetButton: () -> () = { }
+}
+
+public final class RaspberryPi3GPIO: LockGPIOController {
+    
+    public init() {
+        self.resetSwitchGPIO.onRaising { [weak self] in
+            if $0.value == 1 {
+                self?.didPressResetButton()
+            }
+        }
+    }
+    
+    internal lazy var relayGPIO: GPIO = {
+        let gpio = RaspberryGPIO(name:"GPIO23", id:23, baseAddr:0x3F000000)
+        gpio.direction = .OUT
+        gpio.value = 0
+        return gpio
+    }()
+    
+    internal lazy var ledGPIO: GPIO = {
+        let gpio = RaspberryGPIO(name:"GPIO16", id:16, baseAddr:0x3F000000)
+        gpio.direction = .OUT
+        gpio.value = 0
+        return gpio
+    }()
+    
+    internal lazy var resetSwitchGPIO: GPIO = {
+        let gpio = RaspberryGPIO(name:"GPIO12", id:5, baseAddr:0x3F000000)
+        gpio.direction = .IN
+        gpio.value = 0
+        return gpio
+    }()
+    
+    public var relay: GPIOState {
+        get { return GPIOState(rawValue: relayGPIO.value) ?? .off }
+        set { relayGPIO.value = newValue.rawValue }
+    }
+    
     public var led: GPIOState {
         get { return GPIOState(rawValue: ledGPIO.value) ?? .off }
         set { ledGPIO.value = newValue.rawValue }
