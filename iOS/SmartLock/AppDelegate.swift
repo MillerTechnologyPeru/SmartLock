97592c9c26526e9a865ec26503e2066c9f54bd96
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-08-21 23:27:36 -0500

    Updated logging

--- iOS/SmartLock/AppDelegate.swift
+++ iOS/SmartLock/AppDelegate.swift
@@ -36,6 +36,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         // setup logging
         LockManager.shared.log = { log("🔒 \(LockManager.self): " + $0) }
         BeaconController.shared.log = { log("📶 \(BeaconController.self): " + $0) }
+        SpotlightController.shared.log = { log("🔦 \(SpotlightController.self): " + $0) }
         if #available(iOS 10.0, *) {
             UserNotificationCenter.shared.log = { log("📨 \(UserNotificationCenter.self): " + $0) }
         }
@@ -52,7 +53,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
                 mainQueue { self.handle(activity: activity) }
             }
         }
-        
+                
         // handle url
         if let url = launchOptions?[.url] as? URL {
             guard open(url: url)
@@ -65,19 +66,27 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        log("📱 Will resign active")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        log("📱 Did enter background")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        log("📱 Will enter foreground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        log("📱 Did become active")
         
         // update cache if modified by extension
         Store.shared.loadCache()
@@ -85,6 +94,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        log("📱 Will terminate")
     }
     
     func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
