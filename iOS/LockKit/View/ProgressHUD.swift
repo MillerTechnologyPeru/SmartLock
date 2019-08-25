1a6151967ededfa20946228551ed3766f28c26a2
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-08-24 23:08:55 -0500

    Removed Carthage dependencies

--- iOS/LockKit/View/ProgressHUD.swift
+++ iOS/LockKit/View/ProgressHUD.swift
@@ -12,9 +12,14 @@ import JGProgressHUD
 public extension JGProgressHUD {
     
     static func currentStyle(for viewController: UIViewController) -> JGProgressHUD {
-        return JGProgressHUD(userInterfaceStyle: viewController.traitCollection.userInterfaceStyle)
+        if #available(iOS 12.0, iOSApplicationExtension 12.0, *) {
+            return JGProgressHUD(userInterfaceStyle: viewController.traitCollection.userInterfaceStyle)
+        } else {
+            return JGProgressHUD(style: .dark) // default for light environment
+        }
     }
     
+    @available(iOS 12.0, iOSApplicationExtension 12.0, *)
     convenience init(userInterfaceStyle style: UIUserInterfaceStyle) {
         self.init(style: .init(userInterfaceStyle: style))
     }
@@ -22,6 +27,7 @@ public extension JGProgressHUD {
 
 public extension JGProgressHUDStyle {
     
+    @available(iOS 12.0, iOSApplicationExtension 12.0, *)
     init(userInterfaceStyle: UIUserInterfaceStyle) {
         
         switch userInterfaceStyle {
