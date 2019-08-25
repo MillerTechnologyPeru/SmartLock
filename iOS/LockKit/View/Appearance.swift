50fd9379ac45aae82fdd99c8fd908030acf7dff5
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-08-24 23:28:50 -0500

    Support Dark Mode and Large Navigation Bar in iOS 13

--- iOS/LockKit/View/Appearance.swift
+++ iOS/LockKit/View/Appearance.swift
@@ -14,15 +14,42 @@ public extension UIView {
     /// Configure the application's UI appearance
     static func configureLockAppearance() {
         
-        if #available(iOS 11.0, *) {
-            #if targetEnvironment(macCatalyst)
-            UINavigationBar.appearance().prefersLargeTitles = false
-            #endif
-            UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        UINavigationBar.configureAppearance()
+        UITabBar.configureAppearance()
+    }
+}
+
+internal extension UINavigationBar {
+    
+    static func configureAppearance() {
+        
+        let barTintColor = StyleKit.wirelessBlue
+        let tintColor: UIColor = .white
+        let titleTextAttributes: [NSAttributedString.Key : Any] = [
+            .foregroundColor: UIColor.white
+        ]
+        
+        if #available(iOS 13.0, *) {
+            let appearance = UINavigationBarAppearance()
+            appearance.backgroundColor = barTintColor
+            appearance.titleTextAttributes = titleTextAttributes
+            appearance.largeTitleTextAttributes = titleTextAttributes
+            self.appearance().standardAppearance = appearance
+            self.appearance().compactAppearance = appearance
+            self.appearance().scrollEdgeAppearance = appearance
         }
-        UINavigationBar.appearance().tintColor = .white
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
-        UINavigationBar.appearance().barTintColor = StyleKit.wirelessBlue
-        UITabBar.appearance().tintColor = StyleKit.wirelessBlue
+        
+        self.appearance().titleTextAttributes = titleTextAttributes
+        self.appearance().barTintColor = barTintColor
+        self.appearance().largeTitleTextAttributes = titleTextAttributes
+        self.appearance().tintColor = tintColor
+    }
+}
+
+internal extension UITabBar {
+    
+    static func configureAppearance() {
+        
+        self.appearance().tintColor = StyleKit.wirelessBlue
     }
 }
