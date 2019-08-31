1b1b44858fef2bca79ae8a7106ecec25d5193c10
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-08-31 10:26:27 -0500

    Added `LockEvent` Codable implementation

--- Sources/CoreLock/Event.swift
+++ Sources/CoreLock/Event.swift
@@ -15,6 +15,75 @@ public enum LockEvent: Equatable {
     case confirmNewKey(ConfirmNewKey)
 }
 
+// MARK: - Codable
+
+extension LockEvent: Codable {
+    
+    internal enum CodingKeys: String, CodingKey {
+        case type
+        case event
+    }
+    
+    public init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let type = try container.decode(EventType.self, forKey: .type)
+        switch type {
+        case .setup:
+            let event = try container.decode(Setup.self, forKey: .event)
+            self = .setup(event)
+        case .unlock:
+            let event = try container.decode(Unlock.self, forKey: .event)
+            self = .unlock(event)
+        case .createNewKey:
+            let event = try container.decode(CreateNewKey.self, forKey: .event)
+            self = .createNewKey(event)
+        case .confirmNewKey:
+            let event = try container.decode(ConfirmNewKey.self, forKey: .event)
+            self = .confirmNewKey(event)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type, forKey: .type)
+        switch self {
+        case let .setup(event):
+            try container.encode(event, forKey: .event)
+        case let .unlock(event):
+            try container.encode(event, forKey: .event)
+        case let .createNewKey(event):
+            try container.encode(event, forKey: .event)
+        case let .confirmNewKey(event):
+            try container.encode(event, forKey: .event)
+        }
+    }
+}
+
+// MARK: - Supporting Types
+
+public extension LockEvent {
+    
+    enum EventType: String, Codable {
+        
+        case setup = "com.colemancda.Lock.Event.Setup"
+        case unlock = "com.colemancda.Lock.Event.Unlock"
+        case createNewKey = "com.colemancda.Lock.Event.CreateNewKey"
+        case confirmNewKey = "com.colemancda.Lock.Event.ConfirmNewKey"
+    }
+    
+    var type: EventType {
+        
+        switch self {
+        case .setup:            return .setup
+        case .unlock:           return .unlock
+        case .createNewKey:     return .createNewKey
+        case .confirmNewKey:    return .confirmNewKey
+        }
+    }
+}
+
 public extension LockEvent {
     
     struct Setup: Codable, Equatable {
