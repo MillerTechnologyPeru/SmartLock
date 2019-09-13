726d9e3ddb20d9d94494346d9d2bc70d64f1cc48
jurjDev <jurjDev@gmail.com>
2019-09-13 18:04:59 -0500

    Working on RasspberryPi Reset Button

--- Sources/lockd/main.swift
+++ Sources/lockd/main.swift
@@ -110,7 +110,7 @@ func run() throws {
             gpio = gpioController
             gpioController.didPressResetButton = {
                 print("Press Button \(Date())")
-                controller?.lockServiceController.authorization.removeAll()
+                try controller?.lockServiceController.authorization.removeAll()
             }
         }
     }
