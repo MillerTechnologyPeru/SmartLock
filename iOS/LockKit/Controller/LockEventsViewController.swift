7cc0a9e7be9deffc6ebb7188f274ad04786ef02b
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-09-14 15:02:00 -0500

    Working on Events

--- iOS/LockKit/Controller/LockEventsViewController.swift
+++ iOS/LockKit/Controller/LockEventsViewController.swift
@@ -175,7 +175,6 @@ public final class LockEventsViewController: TableViewController {
             }
         }, completion: { (viewController, _) in
             viewController.needsKeys.removeAll()
-            viewController.tableView.reloadData()
         })
     }
     
