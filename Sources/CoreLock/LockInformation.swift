c57ef6a107cc30114ba2c27a7ddec4b13f9dc6cd
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-09-16 23:38:18 -0700

    Added `LockInformation`

---
+++ Sources/CoreLock/LockInformation.swift
@@ -0,0 +1,39 @@
+//
+//  LockInformation.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 9/16/22.
+//
+
+import Foundation
+
+public struct LockInformation: Equatable, Hashable, Codable {
+    
+    /// Lock identifier
+    public let id: UUID
+    
+    /// Firmware build number
+    public let buildVersion: LockBuildVersion
+    
+    /// Firmware version
+    public let version: LockVersion
+    
+    /// Device state
+    public var status: LockStatus
+    
+    /// Supported lock actions
+    public let unlockActions: Set<UnlockAction>
+    
+    public init(id: UUID,
+                buildVersion: LockBuildVersion = .current,
+                version: LockVersion = .current,
+                status: LockStatus,
+                unlockActions: Set<UnlockAction> = [.default]) {
+        
+        self.id = id
+        self.buildVersion = buildVersion
+        self.version = version
+        self.status = status
+        self.unlockActions = unlockActions
+    }
+}
