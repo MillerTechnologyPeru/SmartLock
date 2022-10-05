ec1e9b7c855b18be53268ccdc3c66d5029e9cb0a
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-10-05 02:43:26 -0700

    [CoreLock] Updated `LockNetService.URL`

--- Sources/CoreLock/Networking/NetServiceURL.swift
+++ Sources/CoreLock/Networking/NetServiceURL.swift
@@ -6,6 +6,10 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+import HTTP
 
 public extension LockNetService {
     
@@ -15,7 +19,7 @@ public extension LockNetService {
         case information
         
         /// List Events
-        case events
+        case events(LockEvent.FetchRequest? = nil)
         
         /// List Keys
         case keys
@@ -25,5 +29,64 @@ public extension LockNetService {
         
         /// Delete Key
         case deleteKey(UUID, KeyType)
+    }
+}
+
+public extension LockNetService.URL {
+    
+    /// Build a Lock Net Service request URL provided the server base URL.
+    func url(for server: URL) -> URL {
+        var url = server
+        // build path
+        pathComponents.forEach {
+            url.appendPathComponent($0)
+        }
+        // add query items
+        let queryItems = self.queryItems
+        if queryItems.isEmpty == false {
+            var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)
+            urlComponents?.queryItems = queryItems
+            url = urlComponents?.url ?? url
+        }
+        return url
+    }
+    
+    var pathComponents: [String] {
+        switch self {
+        case .information:
+            return ["info"]
+        case .events:
+            return ["event"]
+        case .keys:
+            return [KeyType.key.stringValue]
+        case .newKey:
+            return [KeyType.key.stringValue]
+        case let .deleteKey(id, type):
+            return [type.stringValue, id.description]
+        }
+    }
+    
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case let .events(fetchRequest):
+            return fetchRequest?.queryItems ?? []
+        default:
+            return []
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .information:
+            return .get
+        case .events:
+            return .get
+        case .keys:
+            return .get
+        case .newKey:
+            return .post
+        case .deleteKey:
+            return .delete
+        }
     }
 }
